7b9151d77c52d294fd1e7539f69aeca3956eb08c
ashatmishra <ashatmishra@users.noreply.github.com>
2025-06-19 17:35:38 +0800

    Feature: Added note delete feature on long tap.

--- app/src/main/java/com/asha/notezen/presentation/screens/notelist/NoteListScreen.kt
+++ app/src/main/java/com/asha/notezen/presentation/screens/notelist/NoteListScreen.kt
@@ -1,6 +1,7 @@
 package com.asha.notezen.presentation.screens.notelist
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,6 +12,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FloatingActionButton
@@ -18,9 +20,13 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -29,11 +35,40 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.core.graphics.toColorInt
+import com.asha.notezen.domain.model.Note
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun NoteListScreen(navController: NavController, viewModel: NoteListViewModel = hiltViewModel()) {
 
     val noteList by viewModel.state.collectAsState()
+
+    var noteToDelete by remember { mutableStateOf<Note?>(null) }
+
+    //Show confirmation dialog if noteToDelete != null
+
+    noteToDelete?.let { note ->
+        AlertDialog(
+            onDismissRequest = { noteToDelete = null },
+            title = { Text("Delete Note") },
+            text = { Text("Are you sure you want to delete the note?") },
+            confirmButton = {
+                TextButton(onClick = {
+                    viewModel.deleteNote(note)
+                    noteToDelete = null
+                }) {
+                    Text("Delete")
+                }
+            },
+            dismissButton = {
+                TextButton(onClick = {
+                    noteToDelete = null
+                }) {
+                    Text("Cancel")
+                }
+            }
+        )
+    }
 
     Scaffold(floatingActionButton = {
         FloatingActionButton(onClick = { navController.navigate("add_note") }) {
@@ -49,7 +84,13 @@ fun NoteListScreen(navController: NavController, viewModel: NoteListViewModel = 
                     modifier = Modifier
                         .padding(horizontal = 16.dp, vertical = 8.dp)
                         .fillMaxWidth()
-                        .clickable { },
+                        .combinedClickable (
+                            onClick = {
+
+                        }, onLongClick = {
+                            noteToDelete = note
+                            }
+                        ),
                     shape = RoundedCornerShape(12.dp),
                     colors = CardDefaults.cardColors(containerColor = noteColor)
                 ) {
