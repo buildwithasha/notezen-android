8fc0339b96f861471519ba0b8a4f6a8f19cb6e74
ashatmishra <ashatmishra@users.noreply.github.com>
2025-07-16 16:49:54 +0800

    feat: Added Constants file

---
+++ app/src/main/java/com/asha/notezen/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.asha.notezen.utils
+
+object ReminderConstants {
+    const val EXTRA_NOTE_ID = "extra_note_id"
+    const val EXTRA_NOTE_TITLE = "extra_note_title"
+    const val EXTRA_NOTE_CONTENT = "extra_note_content"
+}
